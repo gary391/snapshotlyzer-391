9cef80a843cc03677e4aa848421abe14438af2e9
gary391 <gary391@gmail.com>
2019-03-31 08:26:00 -0700

    List volumes and snapshots. Basic implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -18,14 +18,83 @@ def filter_instances(project):
 
 
 @click.group()
+def cli():
+    """ Shotty manages snapshots"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """ Commands for snapshots"""
+
+
+@snapshots.command('list')  # decorator in python decorate/warp functions
+@click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ". join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+
+@cli.group('volumes')
+def volumes():
+    """ Commands for volumes"""
+
+
+@volumes.command('list')  # decorator in python decorate/warp functions
+@click.option('--project', default=None, help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ". join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+
+@cli.group('instances')
 def instances():
     """ Commands for instances"""
+
+
+# decorator in python decorate/warp functions
+@instances.command('snapshot', help="Create snapshots for all volumes")
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print("Creating snapshots of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snapshotlyzer-391")
+    return
 
 
 @instances.command('list')  # decorator in python decorate/warp functions
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
+
     instances = filter_instances(project)
 
     for i in instances:
@@ -65,4 +134,4 @@ def stop_instances(project):
 
 
 if __name__ == '__main__':
-    instances()
+    cli()
